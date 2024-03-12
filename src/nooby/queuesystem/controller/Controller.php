9c736acb6c842c427ce3b3e3376d98e1d26b5a5b
Claudio T <63529134+iSrDxv@users.noreply.github.com>
2024-03-12 20:45:09 -0300

    Update Controller.php

--- src/nooby/queuesystem/controller/Controller.php
+++ src/nooby/queuesystem/controller/Controller.php
@@ -2,5 +2,13 @@
 
 namespace nooby\queuesystem\controller;
 
+use pocketmine\player\Player;
+
 interface Controller
-{}
+{
+  function add(): bool;
+
+  function delete(): bool;
+
+  function getAll(): array;
+}
