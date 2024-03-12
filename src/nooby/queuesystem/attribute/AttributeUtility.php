6a2983d0035f7af44285f5def5c3a9fbedfbf928
Claudio T <63529134+iSrDxv@users.noreply.github.com>
2024-03-12 20:43:37 -0300

    Update AttributeUtility.php
    :D

--- src/nooby/queuesystem/attribute/AttributeUtility.php
+++ src/nooby/queuesystem/attribute/AttributeUtility.php
@@ -6,9 +6,5 @@ use pocketmine\player\Player;
 
 interface AttributeUtility
 {
-    function add(Player $player): bool;
-
-    function delete(Player $player): bool;
-
-    function getAll(): array;
+    function getPlayer(): Player;
 }
