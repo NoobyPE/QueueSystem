c09c671294738b14dfbe19b2a86f80475276cc78
iSrDxv <srclauyt@gmail.com>
2024-04-01 18:59:23 -0300

    #2

--- src/nooby/queuesystem/attribute/QueueMatchmaking.php
+++ src/nooby/queuesystem/attribute/QueueMatchmaking.php
@@ -12,11 +12,11 @@ final class QueueMatchmaking
      */
     static function calculateEndingElo(int $winner, int $losser): array
     {
-        if ($winner > PHP_INT_MAX && $losser > PHP_INT_MAX) {
+        if ($winner >= PHP_INT_MAX && $losser >= PHP_INT_MAX) {
             return [0, 0];
         }
         $scoreA = 1 / (1 + (pow(10, ($winner - $losser) / 408)));
-        $scoreB = 1 / (1 + pow(10, ($winner - $losser) / 408));
+        $scoreB = 1 / (1 + pow(10, ($losser - $winner) / 408));
 
         $winnerElo = $winner + intval(32 * (0 - $scoreA));
         $losserElo = $losser + intval(32 * (1 - $scoreB));
